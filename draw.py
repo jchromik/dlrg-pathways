5378cda53897b411726c7bd226a52f987d8d540b
Jonas Chromik <jonas.chromik@student.hpi.de>
2020-02-17 10:56:37 +0100

    Add missing dep specs

--- draw.py
+++ draw.py
@@ -5,12 +5,12 @@ g = Digraph('G', filename='pathways.gv')
 
 dep_specs = []
 
-for dep_spec_index in [1, 4, 5, 7, 8]:
+for dep_spec_index in [1, 3, 4, 5, 6, 7, 8, 10]:
     with open(f"data/{dep_spec_index}.json") as dep_spec_file:
         dep_specs.append(json.load(dep_spec_file))
 
 for dep_spec in dep_specs:
-    with g.subgraph(name=f"cluster_{dep_spec['name']}") as c:
+    with g.subgraph(name=f"luster_{dep_spec['name']}") as c:
         c.attr(label=dep_spec['name'])
         for qualification, details in dep_spec['qualifications'].items():
             c.node(qualification, fillcolor=dep_spec['color'], style='filled')
